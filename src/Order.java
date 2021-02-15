5fb949f3e903dd583d2f5b088a531ebbd895fc63
tinayi08 <christinayi827@gmail.com>
2021-02-14 17:15:56 -0800

    added user selection capability for food and alcohol

--- src/Order.java
+++ src/Order.java
@@ -22,21 +22,60 @@ public class Order {
 
     public void orderDrinks() {
         Scanner scan = new Scanner(System.in);
-        System.out.println("\nPlease select from the items above to add to your order or enter 0 to move to the next section. ");
+        System.out.println("\nPlease select from the items above to add to your order or enter 0 to move to the next section.");
         int selectedDrink = scan.nextInt();
         ArrayList<Drink> drinksOrdered = new ArrayList<>();
-        while(selectedDrink != 0) {
+        while (selectedDrink != 0) {
+            //selectedDrink = Validator.drinkSelection(selectedDrink, )
             Drink addDrink = OrderHelper.getDrink(selectedDrink);
             drinksOrdered.add(addDrink);
-            System.out.println("Order has been added. Please add another selection or enter 0 to move to the next section.");
+            System.out.println("Item has been added. Please add another item or enter 0 to move to the next section.");
             selectedDrink = scan.nextInt();
         }
-
         System.out.println("Drinks added:");
         for (Drink d : drinksOrdered) {
             System.out.println(d.drinkAdded());
         }
-
     }
+
+    public void orderFood() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nPlease select from the items above to add to your order or enter 0 to move to the next section.");
+        int selectedFood = scan.nextInt();
+        ArrayList<Food> foodOrdered = new ArrayList<>();
+        while (selectedFood != 0) {
+            //selectedFood = Validator.drinkSelection(selectedDrink, )
+            Food addFood = OrderHelper.getFood(selectedFood);
+            foodOrdered.add(addFood);
+            System.out.println("Item has been added. Please add another item or enter 0 to move to the next section.");
+            selectedFood = scan.nextInt();
+        }
+        System.out.println("Food added:");
+        for (Food f : foodOrdered) {
+            System.out.println(f.foodAdded());
+        }
+    }
+
+    public void orderBooze() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("\nPlease select from the items above to add to your order or enter 0 to move to the next section.");
+        int selectedbooze = scan.nextInt();
+        ArrayList<AlcoholicDrink> boozeOrdered = new ArrayList<>();
+        while (selectedbooze != 0) {
+            //selectedFood = Validator.drinkSelection(selectedDrink, )
+            AlcoholicDrink addBooze = OrderHelper.getBooze(selectedbooze);
+            boozeOrdered.add(addBooze);
+            System.out.println("Item has been added. Please add another item or enter 0 to move to the next section.");
+            selectedbooze = scan.nextInt();
+        }
+        System.out.println("Alcoholic drink added:");
+        for (AlcoholicDrink a : boozeOrdered) {
+            System.out.println(a.boozeAdded());
+        }
+    }
+
+
+
+
 
 }
