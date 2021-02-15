536a8afbb6f97fd42a0d6ddfba912863f99c2a8e
tinayi08 <christinayi827@gmail.com>
2021-02-14 17:13:19 -0800

    added user selection capability for food and alcohol

--- src/OrderHelper.java
+++ src/OrderHelper.java
@@ -35,4 +35,70 @@ public class OrderHelper {
         return drink;
     }
 
+    public static Food getFood(int selectedFoodID) {
+        int id = 0;
+        String name = "";
+        int calories = 0;
+        double price = 0;
+
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("src/Files/Food.txt"));
+            String str;
+            while((str = in.readLine()) != null) {
+                String [] foodInfo = str.split(",");
+                id = Integer.parseInt(foodInfo[0]);
+                if(id == selectedFoodID) {
+                    name = foodInfo[1];
+                    calories = Integer.parseInt(foodInfo[2]);
+                    price = Double.parseDouble(foodInfo[3]);
+                    break;
+                }
+
+            }
+            in.close();
+        } catch (FileNotFoundException exc) {
+            exc.printStackTrace();
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
+       Food food = new Food(id, name, calories, price);
+        return food;
+    }
+
+    public static AlcoholicDrink getBooze (int selectedBoozeID) {
+        int id = 0;
+        String name = "";
+        int calories = 0;
+        double price = 0;
+        String alcohol = "";
+        double percentage = 0;
+
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("src/Files/Booze.txt"));
+            String str;
+
+            while((str = in.readLine()) != null) {
+                String [] boozeInfo = str.split(",");
+                id = Integer.parseInt(boozeInfo[0]);
+                if(id == selectedBoozeID) {
+                    name = boozeInfo[1];
+                    calories = Integer.parseInt(boozeInfo[2]);
+                    price = Double.parseDouble(boozeInfo[3]);
+                    alcohol = boozeInfo[4];
+                    percentage = Double.parseDouble(boozeInfo[5]);
+                    break;
+                }
+
+            }
+            in.close();
+        } catch (FileNotFoundException exc) {
+            exc.printStackTrace();
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
+        AlcoholicDrink booze = new AlcoholicDrink(id, name, calories, price, alcohol, percentage);
+        return booze;
+    }
+
+
 }
