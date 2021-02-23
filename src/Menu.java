f9424f38a21499c7cc6ed72254ff446b378f3e0e
tinayi08 <christinayi827@gmail.com>
2021-02-22 17:55:07 -0800

    updated display methods to return int

--- src/Menu.java
+++ src/Menu.java
@@ -28,25 +28,40 @@ public class Menu {
         this.booze = booze;
     }
 
-    public void displayDrinks() {
-        System.out.println("\nPlease select from the following beverages:");
-        for (Drink d : drinks) {
-            System.out.println(d.toString());
+    public int displayDrinks() {
+        if(drinks.size() > 0) {
+            System.out.println("\nPlease select from the following beverages:");
+            for (Drink d : drinks) {
+                System.out.println(d.toString());
+            }
+            return drinks.size();
         }
+        return 0;
+
     }
 
-    public void displayFood() {
-        System.out.println("\nPlease select from the following food items:");
-        for (Food f : food) {
-            System.out.println(f.toString());
+    public int displayFood() {
+        if(food.size() > 0) {
+            System.out.println("\nPlease select from the following food items:");
+            for (Food f : food) {
+                System.out.println(f.toString());
+            }
+            return food.size();
         }
+        return 0;
+
     }
 
-    public void displayBooze() {
-        System.out.println("\nPlease select from the following alcoholic beverages:");
-        for (AlcoholicDrink b : booze) {
-            System.out.println(b.toString());
+    public int displayBooze() {
+        if(booze.size() > 0) {
+            System.out.println("\nPlease select from the following alcoholic beverages:");
+            for (AlcoholicDrink b : booze) {
+                System.out.println(b.toString());
+            }
+            return booze.size();
         }
+        return 0;
+
     }
 }
 
