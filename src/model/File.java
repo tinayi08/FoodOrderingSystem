8cb5d4f034d501f19ace94ba773c5b9abd349a6a
tinayi08 <christinayi827@gmail.com>
2021-05-03 22:54:36 -0700

    removed text files

--- src/model/File.java
+++ src/model/File.java
@@ -2,15 +2,8 @@ package model;
 
 public enum File {
 
-    FOOD("src/Files/Food.txt"), DRINKS("src/Files/Drinks.txt"), BOOZE("src/Files/Booze.txt"), RESTAURANTS("src/Files/Restaurants.txt");
-
-    public String path;
 
 
-
-    File(String path) {
-        this.path = path;
-    }
 
 
 }
